fd9db44f2d4e3d7fb2589540bf2b796a7f877570
DinanathThakur <kumardina023@gmail.com>
2018-01-10 23:45:33 +0530

    new page

--- Modules/Home/Resources/views/home/accreditation.blade.php
+++ Modules/Home/Resources/views/home/accreditation.blade.php
@@ -2,6 +2,123 @@
 @section('title','Accreditation')
 @section('content')
 
+<section class="page-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h1>Accreditation</h1>
+					<ul class="list-unstyled">
+						<li><a href="#">Home</a></li>
+						<li class="active">Accreditation</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section><!-- Ends: .page-header -->
+	
+	<!--============================
+		About Page Content
+	=============================-->
+	<section class="about-ds">
+		<div class="container">
+			<div class="row">
+	            <div class="col-md-12 col-sm-12 about-ds-content">
+					<div class="section-header03">
+						<h2><span>Overview</span></h2>
+					</div>
+					<p>At its most fundamental, knowing that a training institute or program is accredited provides some guarantee that the educational experience you will gain will deliver value after you enrol. This also means that the quality of work you do can be widely appreciated - whether with your current organization, or any other you may choose to work with in future. We are currently associated with a range of accreditation bodies worldwide that include:</p>
+				
+            	</div>
+			</div>
+			<div class="row">
+            <div class="col-md-4 col-sm-6 accrediation-thumbnail ">
+                <div class="bg-white text-center">
+                    <i class="accrediation-icons acdtn-home-1"></i>
+                </div>
+                <span>MyEdulogy is a Global Registered Education Provider (REP) of PMI<sup>®</sup>, USA. Our REP ID is 3757.</span>
+            </div>
+            <div class="col-md-4 col-sm-6 accrediation-thumbnail ">
+                <div class="bg-white text-center">
+                    <i class="accrediation-icons acdtn-home-2"></i>
+                </div>
+                <span>MyEdulogy is an Authorized Training Partner (ATP) and Accredited Training Center (ATC) of EC-Council.</span>
+            </div>
+            <div class="col-md-4 col-sm-6 accrediation-thumbnail ">
+                <div class="bg-white text-center">
+                    <i class="accrediation-icons acdtn-home-3"></i>
+                </div>
+                <span>MyEdulogy is a Registered Education Provider (REP) of Scrum Alliance, Inc. Scrum Alliance is the governing body for CSM and CSPO certifications.</span>
+            </div>
+            <div class="col-md-4 col-sm-6 accrediation-thumbnail ">
+                <div class="bg-white text-center">
+                    <i class="accrediation-icons acdtn-home-4"></i>
+                </div>
+                <span>Accredited Training Provider (ATP) of CompTIA.</span>
+            </div>
+            <div class="col-md-4 col-sm-6 accrediation-thumbnail ">
+                <div class="bg-white text-center">
+                    <i class="accrediation-icons acdtn-home-5"></i>
+                </div>
+                <span>MyEdulogy is an Endorsed Education Provider of IIBA<sup>®</sup></span>
+            </div>
+            <div class="col-md-4 col-sm-6 accrediation-thumbnail ">
+                <div class="bg-white text-center">
+                    <i class="accrediation-icons acdtn-home-7"></i>
+                </div>
+                <span>MyEdulogy is a silver training partner of Scaled Agile Inc.</span>
+            </div>
+            <div class="col-md-4 col-sm-6 accrediation-thumbnail ">
+                <div class="bg-white text-center">
+                    <i class="accrediation-icons acdtn-home-8"></i>
+                </div>
+                <span>MyEdulogy is a Registered Education Partner (REP) of the DevOps Institute (DOI).</span>
+            </div>
+            <div class="col-md-4 col-sm-6 accrediation-thumbnail ">
+                <div class="bg-white text-center">
+                    <i class="accrediation-icons acdtn-home-9"></i>
+                </div>
+                <span>MyEdulogy is an Accredited Examination Centre of IASSC</span>
+            </div>
+            <div class="col-md-4 col-sm-6 accrediation-thumbnail ">
+                <div class="bg-white text-center">
+                    <i class="accrediation-icons acdtn-home-10"></i>
+                </div>
+                <span>MyEdulogy is an ATO of PEOPLECERT</span>
+            </div>
+            <div class="col-md-4 col-sm-6 accrediation-thumbnail ">
+                <div class="bg-white text-center">
+                    <i class="accrediation-icons acdtn-home-11"></i>
+                </div>
+                <span> MyEdulogy is a Certified Partner of AXELOS</span>
+            </div>
+            <div class="col-md-4 col-sm-6 accrediation-thumbnail ">
+                <div class="bg-white text-center">
+                    <i class="accrediation-icons acdtn-home-12"></i>
+                </div>
+                <span> MyEdulogy is a Bronze Partner of the Disciplined Agile Consortium</span>
+            </div>
+            <div class="col-md-4 col-sm-6 accrediation-thumbnail ">
+                <div class="bg-white text-center">
+                    <i class="accrediation-icons acdtn-home-13"></i>
+                </div>
+                <span>MyEdulogy is a Registered Education Partner (REP) of the ISTQB<sup>®</sup> Foundation Training.</span>
+            </div>
+            <div class="col-md-4 col-sm-6 accrediation-thumbnail ">
+                <div class="bg-white text-center">
+                    <i class="accrediation-icons acdtn-home-14"></i>
+                </div>
+                <span>MyEdulogy is an ICAgile Member Training Organization.</span>
+            </div>
+            <div class="col-md-4 col-sm-6 accrediation-thumbnail ">
+                <div class="bg-white text-center">
+                    <i class="accrediation-icons acdtn-home-15"></i>
+                </div>
+                <span>MyEdulogy is a Professional Training Network member of scrum.org</span>
+            </div>
 
+        </div>
+		</div>
+	</section><!-- Ends: .about-ds -->
+	
 
 @stop
--- Modules/Home/Resources/views/home/contact-us.blade.php
+++ Modules/Home/Resources/views/home/contact-us.blade.php
@@ -2,6 +2,78 @@
 @section('title','Contact Us')
 @section('content')
 
-
+<section class="page-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h1>Contact</h1>
+					<ul class="list-unstyled">
+						<li><a href="#">Home</a></li>
+						<li class="active">Contact</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section><!-- Ends: .page-header -->
+	
+	<!--============================
+		Contact Content
+	=============================-->
+	<section class="contact-wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 contact-three-header">
+					<h2>Get In Touch With Us</h2>
+				</div>
+				<div class="col-sm-12 contact-three-info">
+					<ul class="list-unstyled row">
+						<li class="col-sm-4">
+							<span class="icon"><i class="fa fa-phone"></i></span>
+							<span>Phone:  +2034852989</span>
+							<span>Phone:  +91 805 002 1515</span>
+							<span>Phone:  +65 94555425</span>
+						</li>
+						<li class="col-sm-4">
+							<span class="icon"><i class="fa fa-envelope"></i></span>
+							<span>info@myedulogy.com</span>
+							<span>www.myedulogy.com</span>
+						</li>
+						<li class="col-sm-4">
+							<span class="icon"><i class="fa fa-map-marker"></i></span>
+							<span>USA:181 Castle Dr, Stratford, CT 06614</span>
+	<span>INDIA:152, 20th Cross, 17th Main, Sector 3, HSR Layout, Bangalore 560102.</span>
+<span>SINGAPORE:20,Maxwell Road #09-17 Maxwell House Singapore 069113</span>
+						</li>
+					</ul>
+				</div><!-- Ends: .contact-three-info -->
+				<div class="col-sm-12">
+					<div class="contact-two-bottom">
+						<div class="row">
+							<div class="col-sm-12">
+								<h3>Write Us</h3>
+								<form  id="ajax-contact" method="post" action="http://ecologytheme.com/theme/edulight/send.php">
+									<div class="form-group row">
+										<div class="col-sm-6">
+											<input type="text" placeholder="Name *">
+										</div>
+										<div class="col-sm-6">
+											<input type="email" placeholder="Email *">
+										</div>
+										<div class="col-sm-12">
+											<textarea placeholder="Message *"></textarea>
+										</div>
+										<div class="col-sm-12">
+											<button type="submit">Send Message</button>
+										</div>
+									</div>
+									<div id="form-messages"></div>
+								</form>
+							</div><!-- Ends: .col-sm-8 -->
+						</div>
+					</div><!-- Ends: .contact-two-bottom -->
+				</div><!-- Ends: .col-sm-12 -->
+			</div>
+		</div>
+	</section><!-- Ends: .contact-wrapper -->
 
 @stop
